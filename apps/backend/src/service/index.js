32233b5c8e732518bcbbb065f8958874be2ed909
Kartik Sharma <kartiksharma88103@gmail.com>
2025-08-13 19:30:24 +0530

    completed get friend api

--- apps/backend/src/service/index.js
+++ apps/backend/src/service/index.js
@@ -2,4 +2,5 @@ export * from './auth/Signup.js'
 export * from './auth/Login.js'
 export * from './connection/FriendRequest.js'
 export * from './connection/RequestResponse.js'
+export * from './connection/GetFriend.js'
 
